ea6e61b441495cc438253f5fcfc50191e40a55c5
KristinaKuzmenko <kristina_neskey@icloud.com>
2023-12-20 22:38:57 +0200

    bithdays_func_fixed
    added bithdays command to handler
    function birthdays fixed
    new errors added

--- func_get_birthday.py
+++ func_get_birthday.py
@@ -1,4 +1,4 @@
-from Classes.Classes_for_secretary import Birthday
+
 from datetime import datetime
 from collections import defaultdict
 
@@ -6,30 +6,23 @@ from collections import defaultdict
 def get_birthdays_in_days(self, days_ahead):
     today = datetime.today().date()
     upcoming_birthdays = defaultdict(list)
+    for name, record in self.data.items():
+        if record.birthday is not None:
+            birthday = record.birthday.value
+            birthday_this_year = birthday.replace(year=today.year)
 
-    for records in self.data.values():
-        for record in records:
-            if record.birthday:
-                name = record.name.value
-                birthday = datetime.strptime(
-                    record.birthday.value, Birthday.DATE_FORMAT
-                ).date()
-                birthday_this_year = birthday.replace(year=today.year)
+            if birthday_this_year < today:
+                birthday_this_year = birthday.replace(year=today.year + 1)
 
-                if birthday_this_year < today:
-                    birthday_this_year = birthday_this_year.replace(year=today.year + 1)
+            delta_days = (birthday_this_year - today).days
 
-                delta_days = (birthday_this_year - today).days
+            if 0 <= delta_days <= int(days_ahead):
+                upcoming_birthdays[name].append((birthday_this_year, delta_days))
 
-                if 0 <= delta_days <= days_ahead:
-                    upcoming_birthdays[name].append((birthday_this_year, delta_days))
-
-        if upcoming_birthdays:
-            print(f"Upcoming birthdays in the next {days_ahead} days:")
-            for name, birthdays in upcoming_birthdays.items():
-                for birthday, delta_days in birthdays:
-                    print(
-                        f"{name}'s birthday is on {birthday.strftime('%Y-%m-%d')}, {delta_days} days from today."
-                    )
-        else:
-            print(f"No upcoming birthdays in the next {days_ahead} days.")
+    if upcoming_birthdays:
+        print(f"Upcoming birthdays in the next {days_ahead} days:")
+        for name, birthdays in upcoming_birthdays.items():
+            for birthday, delta_days in birthdays:
+                print(f"{name}'s birthday is on {birthday.strftime('%d %B %Y')}, {delta_days} days from today.")
+    else:
+        print(f"No upcoming birthdays in the next {days_ahead} days.")
