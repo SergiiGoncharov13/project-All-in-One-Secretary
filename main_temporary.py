64bab6925bfc6a428915d3b2c4b90e54200ce4c7
vladislavdzadevic <vladdzyadevich@gmail.com>
2023-12-20 04:01:50 +0100

    Second commit (small fix)

--- main_temporary.py
+++ main_temporary.py
@@ -1,4 +1,5 @@
-from Classes.Addressbook import AddressBook
+from Classes.Addressbook import AddressBook, Record
+from Classes.Classes_for_secretary import Name
 
 book = AddressBook()
 
@@ -7,64 +8,64 @@ def error_handler(func):
         try:
             return func(*args, **kwargs)
         except KeyError:
-            return "Цей контакт не існує, будь ласка, спробуйте ще раз"
+            return "This contact doesn't exist, please try again"
         except ValueError as exception:
-            return exception.args[0]
+            return str(exception)
         except IndexError:
-            return "Цей контакт не може бути доданий, він вже існує"
+            return "This contact cannot be added, it exists already"
         except TypeError:
-            return "Невідома команда, будь ласка, спробуйте ще раз"
+            return "Unknown command or incorrect number of arguments, please try again"
     return inner
 
-def hello(_):
+def hello(*_):
     return "How can I help you?"
-    
-def exit(_):
-    return "Goodbye!"
+
+def exit(*_):
+    return "Good bye!"
 
 def add_contact(name):
-    book.add_record(name)
-    return f"Контакт {name} успішно додано."
+    record = Record(name)
+    book.add_record(record)
+    return f"Contact {name} added successfully."
 
 def change_contact(name, phone, new_phone):
     book.change_contact(name, phone, new_phone)
-    return f"Номер телефону {name} успішно змінено."
+    return f"{name}'s phone number changed successfully."
 
-def show_all(_):
+def show_all(*_):
     return str(book)
 
 def show_phone(name):
     contact = book.find_contact(name)
     if contact:
-        return f"Номери телефону {name}: {', '.join(contact.phones)}"
+        return f"{name}'s phone numbers: {', '.join(contact.phones)}"
     else:
-        return f"Контакт {name} не знайдено."
+        return f"Contact {name} not found."
 
 def add_birthday(name, birthday):
     contact = book.find_contact(name)
     if contact:
         contact.add_birthday(birthday)
-        return f"День народження {name} успішно додано."
+        return f"{name}'s birthday added successfully."
     else:
-        return f"Контакт {name} не знайдено."
+        return f"Contact {name} not found."
 
 def show_birthday(name):
     contact = book.find_contact(name)
     if contact:
         return contact.show_birthday()
     else:
-        return f"Контакт {name} не знайдено."
+        return f"Contact {name} not found."
 
 def save_address_book(filename):
     book.save_to_file(filename)
-    return "Адресна книга успішно збережена."
+    return "Address book saved successfully."
 
 def load_address_book(filename):
     book.read_from_file(filename)
-    return "Адресна книга успішно завантажена."
+    return "Address book loaded successfully."
 
 HANDLERS = {
-
     "hello": hello,
     "close": exit,
     "exit": exit,
@@ -78,20 +79,18 @@ HANDLERS = {
     "load": load_address_book,
 }
 
-
 @error_handler
 def parser_input(user_input):
     cmd, *args = user_input.strip().split(' ')
     try:
         handler = HANDLERS[cmd.lower()]
     except KeyError:
-        if args:
-            cmd = f"{cmd} {args[0]}"
-            args = args[1:]
-        handler = HANDLERS[cmd.lower(), "Unknown command"]
-    return handler, args
-
-
+        return "Unknown command, please try again"
+    
+    try:
+        return handler(*args)
+    except TypeError:
+        return "Incorrect number of arguments, please try again"
 
 def main():
     while True:
@@ -99,13 +98,8 @@ def main():
         if user_input in ("close", "exit"):
             print("Good bye!")
             break
-        handler, *args = parser_input(user_input)
-        result = handler(*args)
-        if not result:
-            print("Good bye!")
-            break
+        result = parser_input(user_input)
         print(result)
-
 
 if __name__ == "__main__":
     main()
